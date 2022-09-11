6547259059983d73fbfc603718c5e86366495261
gustavo9601 <tavo9601@gmail.com>
2022-09-11 17:57:21 -0500

    init project

---
+++ src/main/java/springbootkafka/springbootkafka/configs/KafkaConfiguration.java
@@ -0,0 +1,36 @@
+package springbootkafka.springbootkafka.configs;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfiguration {
+
+    private Map<String, Object> producerProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> createTemplate(){
+        Map<String, Object> senderProps = this.producerProps();
+        ProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(senderProps);
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+}
---
+++ src/main/java/springbootkafka/springbootkafka/controllers/ProducerString.java
@@ -0,0 +1,32 @@
+package springbootkafka.springbootkafka.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api/producer-strings")
+public class ProducerString {
+
+
+    private final Logger logger = LoggerFactory.getLogger(ProducerString.class);
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+
+    @GetMapping
+    public ResponseEntity<String> sendString() {
+        logger.info("Enviando mensaje to Kafka");
+        this.kafkaTemplate.send("topic-strings", "Hola desde Spring Boot a las :\t" + LocalDateTime.now());
+        return ResponseEntity.ok("Mensaje enviado");
+    }
+
+}
